e15558808cb30b029a849fbd3fc56b85a3ad7153
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-22 16:28:26 -0500

    se implementa dependencia jackson Core

---
+++ src/test/java/com/example/bibliotecafuncional/application/service/ResourceServiceTest.java
@@ -0,0 +1,68 @@
+package com.example.bibliotecafuncional.application.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ResourceServiceTest {
+
+    @Test
+    void fillData() {
+    }
+
+    @Test
+    void listResource() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void saveResource() {
+    }
+
+    @Test
+    void updateResource() {
+    }
+
+    @Test
+    void deleteResource() {
+    }
+
+    @Test
+    void deleteAllResources() {
+    }
+
+    @Test
+    void findByAvailability() {
+    }
+
+    @Test
+    void borrowResource() {
+    }
+
+    @Test
+    void returnResource() {
+    }
+
+    @Test
+    void recommendThematicArea() {
+    }
+
+    @Test
+    void recommendTypeOfResource() {
+    }
+
+    @Test
+    void recommendThematicAreaAndTypeOfResource() {
+    }
+
+    @Test
+    void findByAuthor() {
+    }
+
+    @Test
+    void findByName() {
+    }
+}
