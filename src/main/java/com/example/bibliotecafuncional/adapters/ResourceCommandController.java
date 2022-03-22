3741dbaaf2cd1f7a6d9f18362759d591da3273db
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-22 02:02:49 -0500

    se crea QueryController y commandController

--- src/main/java/com/example/bibliotecafuncional/adapters/ResourceCommandController.java
+++ src/main/java/com/example/bibliotecafuncional/adapters/ResourceCommandController.java
@@ -18,28 +18,28 @@ public class ResourceCommandController {
     private final ResourceService resourceService;
 
     // fill Data
-    // localhost:8080/api/resource/fillData
+    // localhost:8080/api/command/resource/fillData
     @PostMapping("/fillData")
     public ResponseEntity<List<ResourceDTO>> saveAll(@RequestBody List<ResourceDTO> resources){
         return new ResponseEntity<>(resourceService.fillData(resources), HttpStatus.OK);
     }
 
     // save Resource
-    // localhost:8080/api/resource/saveResource
+    // localhost:8080/api/command/resource/saveResource
     @PostMapping("/saveResource")
     public ResponseEntity<ResourceDTO> saveResource(@RequestBody ResourceDTO resourceDTO){
         return new ResponseEntity<>(resourceService.saveResource(resourceDTO), HttpStatus.CREATED);
     }
 
     // update Resource
-    // localhost:8080/api/resource/updateResource
+    // localhost:8080/api/command/resource/updateResource
     @PutMapping("/updateResource")
     public ResponseEntity<ResourceDTO> updateResource(@RequestBody ResourceDTO resourceDTO ){
         return new ResponseEntity<>(resourceService.updateResource(resourceDTO), HttpStatus.ACCEPTED);
     }
 
     // delete Resource
-    // localhost:8080/api/resource/deleteResourceById?id={}
+    // localhost:8080/api/command/resource/deleteResourceById?id={}
     @DeleteMapping("/deleteResourceById")
     public ResponseEntity<HttpStatus> deleteResource(@RequestParam String id){
         resourceService.deleteResource(id);
@@ -47,7 +47,7 @@ public class ResourceCommandController {
     }
 
     // delete All
-    // localhost:8080/api/resource/deleteAll
+    // localhost:8080/api/command/resource/deleteAll
     @DeleteMapping("/deleteAll")
     public ResponseEntity<HttpStatus> deleteAllResources(){
         resourceService.deleteAllResources();
@@ -55,14 +55,14 @@ public class ResourceCommandController {
     }
 
     // borrow resource
-    // localhost:8080/api/resource/borrowResource?borrow={}
+    // localhost:8080/api/command/resource/borrowResource?borrow={}
     @PutMapping("/borrowResource")
     public ResponseEntity<String> borrowResource(@RequestParam(name="borrow") String id){
         return new ResponseEntity<>(resourceService.borrowResource(id), HttpStatus.ACCEPTED );
     }
 
     // return resource
-    // localhost:8080/api/resource/returnResource?ret={}
+    // localhost:8080/api/command/resource/returnResource?ret={}
     @PutMapping("/returnResource")
     public ResponseEntity<String> returnResource(@RequestParam(name="ret") String id){
         return new ResponseEntity<>(resourceService.returnResource(id), HttpStatus.ACCEPTED );
--- src/main/java/com/example/bibliotecafuncional/adapters/ResourceQueryController.java
+++ src/main/java/com/example/bibliotecafuncional/adapters/ResourceQueryController.java
@@ -24,56 +24,56 @@ public class ResourceQueryController {
     private final ResourceService resourceService;
 
     // list Resource
-    // localhost:8080/api/resource/listResource
+    // localhost:8080/api/query/resource/listResource
     @GetMapping("/listResource")
     public ResponseEntity<List<ResourceDTO>> listResource(){
         return new ResponseEntity<>(resourceService.listResource(), HttpStatus.OK);
     }
 
     // find by id
-    // localhost:8080/api/resource/findById?id={id}
+    // localhost:8080/api/query/resource/findById?id={id}
     @GetMapping("/findById")
     public ResponseEntity<Optional<ResourceDTO>> findById(@RequestParam String id){
         return new ResponseEntity<>(resourceService.findById(id), HttpStatus.OK);
     }
 
     // find By availability
-    // localhost:8080/api/resource/findByAvailability?avail={}
+    // localhost:8080/api/query/resource/findByAvailability?avail={}
     @GetMapping("/findByAvailability")
     public ResponseEntity<List<ResourceDTO>> findByAvailability(@RequestParam(name="avail") Availability availability){
         return new ResponseEntity<>(resourceService.findByAvailability(availability), HttpStatus.OK);
     }
 
     // recommend By ThematicArea
-    // localhost:8080/api/resource/findByThematicArea?thema={}
+    // localhost:8080/api/query/resource/findByThematicArea?thema={}
     @GetMapping("/findByThematicArea")
     public ResponseEntity<List<ResourceDTO>> recommendThematicArea(@RequestParam(name="thema") ThematicArea thematicArea){
         return new ResponseEntity<>(resourceService.recommendThematicArea(thematicArea), HttpStatus.OK);
     }
 
     // recommend By TypeOfResource
-    // localhost:8080/api/resource/findByTypeOfResource?type={}
+    // localhost:8080/api/query/resource/findByTypeOfResource?type={}
     @GetMapping("/findByTypeOfResource")
     public ResponseEntity<List<ResourceDTO>>recommendTypeOfResource(@RequestParam(name="type") TypeOfResource typeOfResource){
         return new ResponseEntity<>(resourceService.recommendTypeOfResource(typeOfResource), HttpStatus.OK);
     }
 
     // recommend by both (ThematicArea, TypeOfResource)
-    // localhost:8080/api/resource/findByThematicAndTypeOfResource?typeOf={}&themat={}
+    // localhost:8080/api/query/resource/findByThematicAndTypeOfResource?typeOf={}&themat={}
     @GetMapping("/findByThematicAndTypeOfResource")
     public ResponseEntity<List<ResourceDTO>> recommendThematicAreaAndTypeOfResource(@RequestParam(name="typeOf") TypeOfResource typeOfResource, @RequestParam(name="themat") ThematicArea thematicArea){
         return new ResponseEntity<>(resourceService.recommendThematicAreaAndTypeOfResource(thematicArea, typeOfResource), HttpStatus.OK);
     }
 
     // find By Author
-    // localhost:8080/api/resource/findByAuthor?au={}
+    // localhost:8080/api/query/resource/findByAuthor?au={}
     @GetMapping("/findByAuthor")
     public ResponseEntity<List<ResourceDTO>>findByAuthor(@RequestParam(name="au") String author){
         return new ResponseEntity<>(resourceService.findByAuthor(author), HttpStatus.OK);
     }
 
     // find by name
-    // localhost:8080/api/resource/findByName?na={}
+    // localhost:8080/api/query/resource/findByName?na={}
     @GetMapping("/findByName")
     public ResponseEntity<List<ResourceDTO>>findByName(@RequestParam(name="na") String name){
         return new ResponseEntity<>(resourceService.findByName(name), HttpStatus.OK);
